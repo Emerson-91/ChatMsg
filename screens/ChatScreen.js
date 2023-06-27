e911303c3d40314821f21713ebeade9c845e7893
Emerson-91 <emerson.hcdossantos@gmail.com>
2023-06-26 21:27:53 -0400

    Editando a tela ChatScreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { Avatar } from '@rneui/themed';
+import FeatherIcons from 'react-native-vector-icons/Feather';
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route }) => {
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Chat",
+      headerTitleAlign: "left",
+      headerTitle: () => (
+        <View style={styles.title}>
+          <Avatar rounded source={{
+            uri: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+          }} />
+          <Text style={styles.titleNome}>{route.params.chatName}</Text>
+        </View>
+      ),
+      headerRight: () => (
+        <View>
+          <TouchableOpacity>
+            <FeatherIcons name="video" size={24} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+      )
+    })
+    
+  }, [navigation])
   return (
     <View>
-      <Text>ChatScreen</Text>
+      <Text>{route.params.chatName}</Text>
     </View>
   )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  title: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  titleNome: {
+    color: "#FFF",
+    marginLeft: 10,
+    fontWeight: "700",
+  }
+})
